e11dcba8bb69660131e0979ca66d794c03047fef
peppamint-png <dhouston7516@sdsu.edu>
2024-02-14 10:13:14 -0800

    fixed employee id

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault();
 
         // GET THE VALUES FROM THE TEXT BOXES
-        const employeeId = document.getElementById('id').value;
+        const id = document.getElementById('id').value;
         const name = document.getElementById('name').value;
         const extension = document.getElementById('extension').value;
         const email = document.getElementById('email').value;
